8e90d959f92950abd6fb3be43efed941de9a28c1
GNOGz <99389733+GNOGz@users.noreply.github.com>
2025-02-16 19:48:17 +0700

    add text area component

---
+++ src/app/components/TextArea.tsx
@@ -0,0 +1,9 @@
+const TextArea = () =>{
+    return(
+        <textarea className="p-2">
+
+        </textarea>
+    );
+}
+
+export default TextArea;
--- src/app/hex/page.tsx
+++ src/app/hex/page.tsx
@@ -1,39 +1,15 @@
 "use client";
-import {
-  HexGrid,
-  Layout,
-  Hexagon,
-  Text,
-  Pattern,
-  Path,
-  Hex,
-  GridGenerator,
-} from "react-hexgrid";
-import "../App.css";
+
+import TextArea from "../components/TextArea";
 
 const greet = () =>{
   alert("Hello")
 }
 
 const grid = () => {
-  const hexagons:Hex[] = GridGenerator.orientedRectangle(8, 8);
-  // console.log(hexagons);
   return (
-    <div className="bg-slate-500">
-      <HexGrid  width={800} height={800} className="mx-auto text-sm bg-white">
-        <Layout
-          size={{ x: 5, y: 5 }}
-          flat={true}
-          spacing={1.1}
-          origin={{ x: -25, y: -40 }}
-        >
-          {hexagons.map((hex, i) => (
-            <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} onClick={greet}>
-              <Text>{`${hex.q} ${hex.r} ${hex.s} `}</Text>
-            </Hexagon>
-          ))}
-        </Layout>
-      </HexGrid>
+    <div className="bg-slate-500 text-black">
+        <TextArea></TextArea>
     </div>
   );
 };
