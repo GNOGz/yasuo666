943d3d4707cb11d746531012f8132654305a3b8d
Wachirawit Chaiyamat <karumanx003@gmail.com>
2025-02-17 09:38:49 +0700

    fix image issue

--- src/app/components/Hexagon.tsx
+++ src/app/components/Hexagon.tsx
@@ -1,9 +1,11 @@
   import "./css/hexagon.css";
-import MMT1 from '../image/MMT1.jpg'; 
-import MMT2 from '../image/MMT2.jpg'; 
-import MMT3 from '../image/MMT3.jpg'; 
-import MMT4 from '../image/MMT4.jpg'; 
-import MMT5 from '../image/MMT5.jpg';//Issue about image
+import MMT1 from '/image/MMT1.jpg'; 
+import MMT2 from '/image/MMT2.jpg'; 
+import MMT3 from '/image/MMT3.jpg'; 
+import MMT4 from '/image/MMT4.jpg'; 
+import MMT5 from '/image/MMT5.jpg';//Issue about image
+
+const pitures = ['/image/MMT1.jpg', '/image/MMT2.jpg','/image/MMT3.jpg','/image/MMT4.jpg',,'/image/MMT5.jpg']
 type data = [string,boolean,number]; // color [P1,P2,Empty] MM [Null,0,1] MMtype [Null,1,2,3,4,5]  
 let datas: data[][] = [];
 export const Hexagon = () => {
@@ -14,7 +16,7 @@ export const Hexagon = () => {
         <div>
         <div className="hexagonP1"></div>
         <div className="hexagonMMB"></div>
-        <img className="hexagonMM" src="https://i1.sndcdn.com/artworks-000065334969-gmnp3t-t500x500.jpg" alt="Minion"></img> 
+        <img className="hexagonMM" src={pitures[0]} alt="Minion"></img> 
         <div className="hexagonMMBP1"></div>
         <div className="hexagon"></div>
         </div>
