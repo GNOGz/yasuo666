ce8ac6dc3cb08e3779af55c139ce491e571824e6
GNOGz <99389733+GNOGz@users.noreply.github.com>
2025-03-23 21:39:02 +0700

    Fix player2 name for solitaire

--- src/app/mode/solitaire/page.tsx
+++ src/app/mode/solitaire/page.tsx
@@ -4,6 +4,10 @@ import { JetBrains_Mono } from "next/font/google";
 import { useState,useEffect } from "react";
 import { Client } from "@stomp/stompjs";
 import SolitairJoinMenu from "@/app/components/SolitaireJoinMenu";
+import { useDispatch } from "react-redux";
+import api from "@/app/libs/api";
+import { setPlayer2 } from "@/app/stores/slices/roomDataSlice";
+import { error } from "console";
 
 const mainFont = JetBrains_Mono({
   weight: ["400"],
@@ -11,13 +15,25 @@ const mainFont = JetBrains_Mono({
 });
 
 const solitairPage = () => {
-  
+  const dispatch = useDispatch();
+  const assignPlayer2 = async ()=>{
+    try{
+    const response = await api.get("/getPlayer2");
+    dispatch(setPlayer2(response.data.name));
+    }
+    catch{
+      alert("Cannot connect to the server.")
+    }
+  }
+  useEffect(()=>{
+    assignPlayer2()
+  },[]);
   return (
     <div
       className={`${mainFont.className} flex flex-col gap-8 justify-center items-center text-outline min-h-screen bg-[url(https://preview.redd.it/in73r6sbixz31.png?width=4096&format=png&auto=webp&s=810fa4aef8f17b2ccf3ca4c18601eb731904d37e)] bg-cover bg-no-repeat text-center text-9xl text-white  `}
     >
       <div className="text-5xl">Player(s) Setup</div>
-      <JoinMenu></JoinMenu>
+      <JoinMenu solitaire={true}></JoinMenu>
       <div className="border border-double-2 border-black text-xl text-outline-mini bg-secondary p-3">
         <div>
           <span className="text-red-400">Important : </span> Once your decision is made. You can not undo anything. <br></br>
